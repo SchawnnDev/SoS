c26dbf60536050387318a8da0d6f9e24fa0e94a0
qgerling <quentin.gerling@etu.unistra.fr>
2022-12-12 21:51:39 +0100

    Creation of the initialisation method of the identifier list

--- include/variable.h
+++ include/variable.h
@@ -6,10 +6,12 @@
 #define TMP_MAX 100
 
 /* Types */
+#define UNSET 0
 #define CHAIN 1
 #define INTEGER 2
 #define ARRAY 3
 #define FUNCTION 4
+#define MAXTYPEVALUE 4 /* /!\  A modifier lors de l'ajout ou suppression d'un type */
 
 /* Operations */
 
--- src/listIden.c
+++ src/listIden.c
@@ -1,2 +1,42 @@
 #include "../include/listIden.h"
 
+/*!
+ * \fn ListIdentifier initListIdentifier()
+ * \brief Fonction qui initialise la structure des Identificateurs
+*/
+ListIdentifier initListIdentifier()
+{
+    ListIdentifier addr;
+    CHECKPOINTER(addr = (ListIdentifier)malloc(sizeof(listIdentifier_t)));
+    addr->numbreIden = 0;
+    return addr;
+}
+
+/*!
+ * \fn void cleanListIdentifier(ListIdentifier addr)
+ * \brief Fonction qui libère la mémoire de la structure des Identificateurs
+*/
+void cleanListIdentifier(ListIdentifier addr)
+{
+    free(addr);
+}
+
+/*!
+ * \fn ListIdentifier addIntoListIdentifier(ListIdentifier addr, char* name, char* value)
+ * \brief Fonction qui crée un Identifier et l'ajoute en fin de la liste des identificateurs
+*/
+ListIdentifier addIntoListIdentifier(ListIdentifier addr, char* name, char* value, int type)
+{
+    CHECKPOINTER(addr);
+
+    if((type < UNSET ) || (type > MAXTYPEVALUE)){
+        perror("addIntoListIdentifier : Value du type n'est pas admise par le compilateur.");
+        exit(EXIT_FAILURE);
+    }
+
+    Identifier identifierAddr;
+    CHECKPOINTER(identifierAddr = (Identifier)malloc(sizeof(identifier_t)));
+    int size = strlen( name ) + 1;
+
+    /* To Do*/
+}
