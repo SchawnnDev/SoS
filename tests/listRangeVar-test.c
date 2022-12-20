ef71c3ccfed80f6dec5041a00ea165853449c257
qgerling <quentin.gerling@etu.unistra.fr>
2022-12-20 20:19:02 +0100

    log messages correction and init function tests

--- tests/listRangeVar-test.c
+++ tests/listRangeVar-test.c
@@ -15,12 +15,38 @@ TEST initRangeVariableLvlZeroTest(void) {
     PASS();
 }
 
+TEST initRangeVariableLvlNTest(void) {
+    RangeVariable addr1 = initRangeVariable(0,NULL);
+    RangeVariable addr = initRangeVariable(10,addr1);
+
+    ASSERT_NEQ(NULL, addr);
+    ASSERT_EQ_FMT(10, addr->rangeLevel,"%d");
+    ASSERT_EQ(NULL, addr->nextLevel);
+    ASSERT_EQ(addr1, addr->previousLevel);
+    ASSERT_NEQ(NULL, addr->listIdentifier);
+    PASS();
+}
+
+//-------------------//
+// initListRangeVariable //
+//-------------------//
+TEST initListRangeVariableTest(void) {
+    ListRangeVariable addr = initListRangeVariable(0,NULL);
+
+    ASSERT_NEQ(NULL, addr);
+    ASSERT_NEQ(NULL, addr->cursor);
+    PASS();
+}
+
 GREATEST_MAIN_DEFS();
 
 int main(int argc, char **argv) {
     GREATEST_MAIN_BEGIN();
 
     RUN_TEST(initRangeVariableLvlZeroTest);
+    RUN_TEST(initRangeVariableLvlNTest);
+
+    RUN_TEST(initListRangeVariableTest);
 
     GREATEST_MAIN_END();
 }
