3aed63f9f011bdcaab9a300084ba469d61a88cb9
qgerling <quentin.gerling@etu.unistra.fr>
2022-12-20 21:20:02 +0100

    addIdentifier tests

--- tests/listRangeVar-test.c
+++ tests/listRangeVar-test.c
@@ -1,6 +1,17 @@
 #include <greatest.h>
 #include "listRangeVar.h"
 
+char* names[IDEN_MAX] = {"test0","test1","test2","test3","test4","test5","test6","test7","test8","test9",
+                         "test10","test11","test12","test13","test14","test15","test16","test17","test18","test19",
+                         "test20","test21","test22","test23","test24","test25","test26","test27","test28","test29",
+                         "test30","test31","test32","test33","test34","test35","test36","test37","test38","test39",
+                         "test40","test41","test42","test43","test44","test45","test46","test47","test48","test49",
+                         "test50","test51","test52","test53","test54","test55","test56","test57","test58","test59",
+                         "test60","test61","test62","test63","test64","test65","test66","test67","test68","test69",
+                         "test70","test71","test72","test73","test74","test75","test76","test77","test78","test79",
+                         "test80","test81","test82","test83","test84","test85","test86","test87","test88","test89",
+                         "test90","test91","test92","test93","test94","test95","test96","test97","test98","test99"};
+
 //-------------------//
 // initRangeVariable //
 //-------------------//
@@ -201,6 +212,39 @@ TEST searchIdentifierPositionWithIdentifierOnNLvlInRangeListWithMayLvlAndNotFirs
     PASS();
 }
 
+//--------------------------//
+// addIdentifier //
+//--------------------------//
+TEST addIdentifierTest(void) {
+    ListRangeVariable addr = initListRangeVariable();
+
+    ASSERT_EQ(RETURN_SUCCESS, addIdentifier(addr,"test"));
+    VariablePosition variablePosition = searchIdentifierPosition(addr,"test");
+    ASSERT_NEQ(NULL, variablePosition->rangePosition);
+    ASSERT_EQ(0, variablePosition->indexIdentifier);
+    PASS();
+}
+
+TEST addIdentifierTwiceTest(void) {
+    ListRangeVariable addr = initListRangeVariable();
+
+    ASSERT_EQ(RETURN_SUCCESS, addIdentifier(addr,"test"));
+    ASSERT_EQ(RETURN_FAILURE, addIdentifier(addr,"test"));
+    PASS();
+}
+TEST addIdentifierToManyIdentifierTest(void) {
+    ListRangeVariable addr = initListRangeVariable();
+
+    //simule le remplissage de la liste
+    int i;
+    for(i = 0; i < IDEN_MAX; i++){
+        addIdentifier(addr, names[i]);
+    }
+
+    ASSERT_EQ(RETURN_FAILURE, addIdentifier(addr,"test"));
+    PASS();
+}
+
 
 GREATEST_MAIN_DEFS();
 
@@ -228,6 +272,10 @@ int main(int argc, char **argv) {
     RUN_TEST(searchIdentifierPositionWithIdentifierInRangeListWithMayLvlTest);
     RUN_TEST(searchIdentifierPositionWithIdentifierOnFirstLvlInRangeListWithMayLvlTest);
     RUN_TEST(searchIdentifierPositionWithIdentifierOnNLvlInRangeListWithMayLvlAndNotFirstIdentifierTest);
+
+    RUN_TEST(addIdentifierTest);
+    RUN_TEST(addIdentifierTwiceTest);
+    RUN_TEST(addIdentifierToManyIdentifierTest);
 
     GREATEST_MAIN_END();
 }
