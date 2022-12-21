f8866eaef8c72ab5546e2537aaf918dce737190c
SchawnnDev <iperskill@gmail.com>
2022-12-21 01:14:33 +0100

    changed cmake building + added apt-get installs flex & bison

--- install.sh
+++ install.sh
@@ -14,8 +14,31 @@ else
   echo "Mars already downloaded! Skipping to next step."
 fi;
 
-echo "Init CMake"
-cmake $CMAKE_BUILD_FOLDER
+if [ $(dpkg-query -W -f='${Status}' flex 2>/dev/null | grep -c "Flex is installed... skipping to next step.") -eq 0 ];
+then
+  echo "Installing flex using apt..."
+  apt-get -y install flex;
+fi
 
-echo "Building CMake"
-cmake --build $CMAKE_BUILD_FOLDER
+if [ $(dpkg-query -W -f='${Status}' bison 2>/dev/null | grep -c "Bison is installed... skipping to next step.") -eq 0 ];
+then
+  echo "Installing bison using apt..."
+  apt-get -y install bison;
+fi
+
+if [ ! -f $CMAKE_BUILD_FOLDER ]; then
+	echo "Creating folder ${CMAKE_BUILD_FOLDER}..."
+	mkdir $CMAKE_BUILD_FOLDER
+	cd $CMAKE_BUILD_FOLDER
+	
+	echo "Starting CMake init..."
+	cmake ..
+	
+	echo "Building CMake..."
+	cmake --build .
+	
+	echo "Building using makefile..."
+	make
+else
+	echo "Folder ${CMAKE_BUILD_FOLDER} already exists... skipping CMake init!"
+fi;
