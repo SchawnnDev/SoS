f8f04703009a5e2f795839c178cff7bf3abc2dd4
SchawnnDev <iperskill@gmail.com>
2022-12-21 01:05:21 +0100

    download working

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ MARS_BIN_NAME=Mars4_5.jar
 echo "Updating submodules..."
 git submodule update --init --recursive
 
-if [ -f $MARS_BIN_NAME ]; then
+if [ ! -f $LIB_DIR$MARS_BIN_NAME ]; then
   echo "Downloading mars to lib folder"
   wget -P $LIB_DIR http://courses.missouristate.edu/KenVollmar/MARS/MARS_4_5_Aug2014/$MARS_BIN_NAME
 else
@@ -15,7 +15,7 @@ else
 fi;
 
 echo "Init CMake"
-cmake . $CMAKE_BUILD_FOLDER
+cmake $CMAKE_BUILD_FOLDER
 
 echo "Building CMake"
 cmake --build $CMAKE_BUILD_FOLDER
