adcee62a460f467cb84ada6469cb97cea1a6d7c5
qgerling <quentin.gerling@etu.unistra.fr>
2022-12-26 15:04:48 +0100

    implementation of initialisation function of listInstruction

--- include/listInstructionMips.h
+++ include/listInstructionMips.h
@@ -24,7 +24,7 @@ typedef struct code_t{
     int numberCode;
     char* lineCode[CODE_TAB_MAX];
     int unDefineGoto[CODE_TAB_MAX];
-    struct code_t* previousCCode;
+    struct code_t* previousCode;
 } *Code;
 
 typedef struct{
@@ -32,5 +32,43 @@ typedef struct{
     Text cursorText;
     Code cursorCode;
 }listInstruction_t, *ListInstruction;
+
+/*!
+ * \fn Data initData( Data previousData)
+ * \brief Fonction qui initialise la structure de data
+ *
+ * \param previousData : Data, la table précédante
+ *
+ * \return Data, un pointeur d'une structure de data
+*/
+Data initData( Data previousData);
+
+/*!
+ * \fn Text initText( Text previousText)
+ * \brief Fonction qui initialise la structure de Text
+ *
+ * \param previousText : Text, la table précédante
+ *
+ * \return Text, un pointeur d'une structure de text
+*/
+Text initText( Text previousText);
+
+/*!
+ * \fn Code initCode( Code previousCode)
+ * \brief Fonction qui initialise la structure de code
+ *
+ * \param previousCode : Data, la table précédante
+ *
+ * \return Code, un pointeur d'une structure de code
+*/
+Code initCode( Code previousCode);
+
+/*!
+ * \fn ListInstruction initListInstruction()
+ * \brief Fonction qui initialise la structure de liste d'instruction
+ *
+ * \return Data, un pointeur d'une structure de data
+*/
+ListInstruction initListInstruction();
 
 #endif //SOS_LISTINSTRUCTIONMIPS_H
--- src/listInstructionMips.c
+++ src/listInstructionMips.c
@@ -1,3 +1,62 @@
-//
-// Created by the_s on 26/12/2022.
-//
+#include "listInstructionMips.h"
+
+/*!
+ * \fn Data initData( Data previousData)
+ * \brief Fonction qui initialise la structure de data
+*/
+Data initData(Data previousData)
+{
+    log_trace("initData (Data %p)",previousData)
+
+    Data addr;
+    CHECKPOINTER(addr = (Data)malloc(sizeof( struct data_t)));
+    addr->previousData = previousData;
+    addr->numberData = 0;
+    return addr;
+}
+
+/*!
+ * \fn Text initText( Text previousText)
+ * \brief Fonction qui initialise la structure de Text
+*/
+Text initText(Text previousText)
+{
+    log_trace("initText (Text %p)",previousText)
+
+    Text addr;
+    CHECKPOINTER(addr = (Text)malloc(sizeof( struct text_t)));
+    addr->previousText = previousText;
+    addr->numberText = 0;
+    return addr;
+}
+
+/*!
+ * \fn Code initCode( Code previousCode)
+ * \brief Fonction qui initialise la structure de code
+*/
+Code initCode(Code previousCode)
+{
+    log_trace("initCode (Code %p)",previousCode)
+
+    Code addr;
+    CHECKPOINTER(addr = (Code)malloc(sizeof( struct code_t)));
+    addr->previousCode = previousCode;
+    addr->numberCode = 0;
+    return addr;
+}
+
+/*!
+ * \fn ListInstruction initListInstruction()
+ * \brief Fonction qui initialise la structure de liste d'instruction
+*/
+ListInstruction initListInstruction()
+{
+    log_trace("initListInstruction (void)")
+
+    ListInstruction addr;
+    CHECKPOINTER(addr = (ListInstruction)malloc(sizeof( listInstruction_t)));
+    addr->cursorData = initData(NULL);
+    addr->cursorText = initText(NULL);
+    addr->cursorCode = initCode(NULL);
+    return addr;
+}
